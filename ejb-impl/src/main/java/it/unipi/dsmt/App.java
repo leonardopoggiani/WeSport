6277de745170ae675ab2fc5c1f1d9d07cc32d3d6
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2022-03-08 23:00:39 +0100

    struttura generale

---
+++ ejb-impl/src/main/java/it/unipi/dsmt/App.java
@@ -0,0 +1,13 @@
+package it.unipi.dsmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
