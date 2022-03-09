e957214c1c0f23a6e24d1778f2f4a6ffc3eb3152
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2022-03-09 19:12:43 +0100

    database sql

---
+++ user_table.sql
@@ -0,0 +1,3 @@
+CREATE TABLE user (ID INT AUTO_INCREMENT, name VARCHAR(35), surname VARCHAR(35),
+       username VARCHAR(35), password VARCHAR(35), email VARCHAR(35), city VARCHAR(35),
+       postal_code int, description VARCHAR(150));  
